f950b8e936eb3d1e92c7b18cbca97ec4173df33b
jjakucionis <jjakucionis@Justass-iMac.lan>
2019-09-24 22:53:07 +0300

    Header markup

---
+++ partials/header.php
@@ -0,0 +1,37 @@
+<header>
+  <div>
+    <a href="#">Paremkite mus</a>
+    <a href="#">Lesė klinika</a>
+    <a href="#">Lesė kamera</a>
+    <div>
+      <a href="#"><span></span> Lietuvių</a>
+      <a href="#"><span></span> English</a>
+      <a href="#"><span></span> Russian</a>
+    </div>
+  </div>
+  <div>
+    <ul>
+      <li><a href="#">Globotiniai</a></li>
+      <li><a href="#">Parama</a>
+        <ul>
+          <li><a href="#">Submeniu 1</a></li>
+          <li><a href="#">Submeniu 2</a></li>
+          <li><a href="#">Submeniu 3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Savanoriauk</a></li>
+    </ul>
+    <a href="#"><img src="./assets/images/lese_logo_baltas.png" alt=""></a>
+    <ul>
+      <li><a href="#">Globotiniai</a></li>
+      <li><a href="#">Parama</a>
+        <ul>
+          <li><a href="#">Submeniu 4</a></li>
+          <li><a href="#">Submeniu 5</a></li>
+          <li><a href="#">Submeniu 6</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Savanoriauk</a></li>
+    </ul>
+  </div>
+</header>
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import '../scss/style.scss';
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
