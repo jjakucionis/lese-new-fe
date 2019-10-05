39b8e83199be6b7978af55b9d72b364560369900
jjakucionis <jjakucionis@Justass-iMac.lan>
2019-10-05 20:47:19 +0300

    About page layout

---
+++ apie.php
@@ -0,0 +1,116 @@
+<?php include './partials/head.php'; ?>
+<?php include './partials/header.php'; ?>
+<main>
+  <section class="hero" style="background-image: url(./assets/images/cat_banner.jpg)">
+    <div class="hero__content">
+      <div class="section-decoration">
+        <div class="section-decoration__line"></div>
+        <svg class="section-decoration__svg" width="551.06px" height="551.06px" enable-background="new 0 0 551.062 551.062" version="1.1" viewBox="0 0 551.062 551.062" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+          <path d="m465.19 453.46c14.749 67.688-58.752 82.375-91.127 73.562s-98.41-10.281-98.41-10.281-66.218 1.469-98.593 10.281c-32.375 8.874-105.94-5.875-91.249-73.562s79.438-64.75 97.186-156c17.687-91.249 92.718-85.374 92.718-85.374s74.847-5.875 92.535 85.374c17.625 91.249 82.252 88.311 96.94 156zm-121.6-247.31c39.841 11.505 83.844-19.951 98.349-70.258 14.504-50.245-5.998-100.31-45.839-111.81-39.842-11.506-83.844 19.951-98.349 70.258-14.504 50.245 5.998 100.31 45.839 111.81zm165.12-18.298c-38.372-15.668-85.496 10.894-105.26 59.363-19.768 48.471-4.712 100.43 33.66 116.04 38.372 15.606 85.496-10.894 105.26-59.364s4.713-100.37-33.66-116.03zm-301.29 18.298c39.841-11.506 60.343-61.567 45.839-111.81s-58.568-81.702-98.349-70.196c-39.78 11.505-60.343 61.566-45.839 111.81 14.505 50.245 58.508 81.701 98.349 70.196zm-93.453 157.1c38.373-15.667 53.428-67.626 33.66-116.04s-66.892-75.031-105.26-59.363c-38.372 15.667-53.427 67.626-33.659 116.03 19.767 48.47 66.891 75.031 105.26 59.364z"/>
+        </svg>
+        <div class="section-decoration__line"></div>
+      </div>
+      <h1 class="section-title section-title--inner">Apie mus</h1>
+      <p class="section-subtext">Lorem ipsum text text lorem ipsum</p>
+    </div>
+  </section>
+  <section class="section company">
+    <div class="container">
+      <div class="company-row">
+        <div class="company-block company-block--left">
+          <h4 class="company-block__title company-block__title--smaller">Nuo 2007 metų</h4>
+          <h3 class="company-block__title company-block__title--large">Gelbėjame gyvūnus ir padedame rasti naujus namus</h3>
+        </div>
+        <div class="company-block company-block--right">
+          <h4 class="company-block__title">Pagrindinė mūsų veikla:</h4>
+          <ul>
+            <li>gyvūnų sterilizacija;</li>
+            <li>laikina gyvūnų globa;</li>
+            <li>naujų namų paieška beglobiams gyvūnams;</li>
+            <li>gyvūnų gelbėjimo darbai bei gydymas;</li>
+            <li>dovanojamų, rastų ir dingusių gyvūnų portalo GyvunuGloba.lt administravimas;</li>
+            <li>beglobių gyvūnų sterilizacijos programos Pagauk-Sterilizuok-Paleisk vykdymas.</li>
+          </ul>
+        </div>
+      </div>
+      <div class="company-row">
+        <div class="company-block company-block--left">
+          <h4 class="company-block__title">Mūsų tikslai:</h4>
+          <ul>
+            <li>Humaniškais būdais sumažinti beglobių gyvūnų skaičių;</li>
+            <li>Padėti surasti naujus namus dovanojamiems gyvūnams;</li>
+            <li>Stabdyti žiaurumą ir netinkamą elgesį su gyvūnais Lietuvoje;</li>
+            <li>Sukurti efektyviai veikiančią gyvūnų gelbėjimo sistemą;</li>
+            <li>Įkurti Europos Sąjungos standartus atitinkančius gyvūnų globos namus;</li>
+            <li>Rengti ir vykdyti visuomenės švietimo programas apie mūsų veiklą ir gyvūnų globą Lietuvoje.</li>
+          </ul>
+        </div>
+        <div class="company-block company-block--right">
+          <h4 class="company-block__title">Mūsų misija:</h4>
+          <ul>
+            <li>Humaniškais būdais sumažinti beglobių gyvūnų skaičių;</li>
+            <li>Padėti surasti naujus namus dovanojamiems gyvūnams;Šviesti visuomenę gyvūnų gerovės klausimais;</li>
+            <li>Kokybiška beglobių gyvūnų priežiūra.</li>
+          </ul>
+          <h4 class="company-block__title">Mūsų vizija:</h4>
+          <p>Organizacija, aktyviai gerinanti visos Lietuvos beglobių gyvūnų situaciją, ginanti gyvūnų teises ir ugdanti Lietuvos visuomenės rimtą požiūrį į gyvūnų globos problemas.</p>
+        </div>
+      </div>
+      <p>Nuoširdžiai tikime, kad visi keturkojai nusipelno antro suteikto šanso, todėl suteikiame jį išmestiems, išduotiems, nebereikalingiems gyvūnams. Gyvūnų globos organizacija „Lesė“ įkurta 2007 metais, šiuo metu įsikūrusi Vilniuje ir Kaune. Savo globotinių nemigdome, jie gyvena pas mus tol, kol randa namus arba miršta nuo senatvės. Organizacijos pagrindą sudaro savanoriai, kurie už tai negauna materialaus atlygio.</p>
+    </div>
+  </section>
+  <section class="section vert">
+    <div class="container">
+      <img src="./assets/images/vertybes.png" alt="Lesė vertybės">
+    </div>
+  </section>
+  <section class="section chart">
+
+  </section>
+  <section class="section numbers" style="background-image: url(./assets/images/people.png)">
+    <div class="container">
+      <div class="numbers-row">
+        <div class="numbers-block">
+          <p class="numbers-block__num">50</p>
+          <p class="numbers-block__tit">Renginių</p>
+        </div>
+        <div class="numbers-block">
+          <p class="numbers-block__num">30</p>
+          <p class="numbers-block__tit">Ekskursijų</p>
+        </div>
+        <div class="numbers-block">
+          <p class="numbers-block__num">15</p>
+          <p class="numbers-block__tit">Pamokų</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section text-content">
+    <div class="text-content-title">
+      <div class="container">
+        <h2>Mūsų veikla</h2>
+      </div>
+    </div>
+    <div class="container">
+      <div class="text-content-cont">
+          <p>Be kasdienių darbų prieglaudoje, dalyvaujame ir įvairiuose renginiuose. Vienas pagrindinių mūsų tikslų – šviesti visuomenę. Renginiai – vieta, kurioje renkasi daug bendraminčių, kurios metu mes, prieglauda, bendraujame su žmonėmis, dalinamės patirtimi, įspūdžiais, padedame suprasti, koks yra atsakingas šeimininkas, kaip siekti gyvūnų gerovės, kokie yra savanoriškos veiklos privalumai, dalinamės gyvūnų priežiūrios patarimais. Jei yra galimybė, vežamės savo globotinius, kurie visiems pakelia nuotaiką, žaidžia su renginio lankytojais, laukia paglostymų ir tikisi kažkam įkristi į akį. Esame dalyvavę įvairiausiuose renginiuose – apie savanorystę, kelionių, studijų mugėse, muzikos festivaliuose, kalėdinėse, kitose tradicinėse šventinėse mugėse.</p>
+          <p>Visada laukiame kvietimų į renginius, kuriuose būtume naudingi. Jeigu noretumėte mus matyti ir išgirsti mūsų istorijas, rašykite el. adresu <a href="mailto:vilnius@lese.lt">vilnius@lese.lt</a></p>
+      </div>
+    </div>  
+  </section>
+  <section class="section text-content">
+    <div class="text-content-title">
+      <div class="container">
+        <h2>Mūsų veikla 2</h2>
+      </div>
+    </div>
+    <div class="container">
+      <div class="text-content-cont">
+          <p>Be kasdienių darbų prieglaudoje, dalyvaujame ir įvairiuose renginiuose. Vienas pagrindinių mūsų tikslų – šviesti visuomenę. Renginiai – vieta, kurioje renkasi daug bendraminčių, kurios metu mes, prieglauda, bendraujame su žmonėmis, dalinamės patirtimi, įspūdžiais, padedame suprasti, koks yra atsakingas šeimininkas, kaip siekti gyvūnų gerovės, kokie yra savanoriškos veiklos privalumai, dalinamės gyvūnų priežiūrios patarimais. Jei yra galimybė, vežamės savo globotinius, kurie visiems pakelia nuotaiką, žaidžia su renginio lankytojais, laukia paglostymų ir tikisi kažkam įkristi į akį. Esame dalyvavę įvairiausiuose renginiuose – apie savanorystę, kelionių, studijų mugėse, muzikos festivaliuose, kalėdinėse, kitose tradicinėse šventinėse mugėse.</p>
+          <p>Visada laukiame kvietimų į renginius, kuriuose būtume naudingi. Jeigu noretumėte mus matyti ir išgirsti mūsų istorijas, rašykite el. adresu <a href="mailto:vilnius@lese.lt">vilnius@lese.lt</a></p>
+      </div>
+    </div>  
+  </section>
+</main>
+<?php include './partials/footer.php'; ?>
+<?php include './partials/foot.php'; ?>
