71d576022b9b8180b4ac35e7592f346a6c5218a2
jjakucionis <jjakucionis@Justass-iMac.lan>
2019-10-09 22:41:50 +0300

    News inner page started

---
+++ news-inner.php
@@ -0,0 +1,65 @@
+<?php include './partials/head.php'; ?>
+<?php include './partials/header.php'; ?>
+<main>
+  <section class="controls controls--padding">
+    <div class="container">
+      <div class="flexwrap">
+        <div class="breadcrumbs">
+          <a class="btn__breadcrumb" href="">Lesė</a>
+          <a class="btn__breadcrumb" href="">Naujienos</a>
+          <a class="btn__breadcrumb" href="">Naujienos pavadinimas</a>
+        </div>
+        <div class="go-back">
+          <a class="btn__back" href="#">
+            <svg enable-background="new 0 0 31.494 31.494" version="1.1" viewBox="0 0 31.494 31.494" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+              <path d="m10.273 5.009c0.444-0.444 1.143-0.444 1.587 0 0.429 0.429 0.429 1.143 0 1.571l-8.047 8.047h26.554c0.619 0 1.127 0.492 1.127 1.111s-0.508 1.127-1.127 1.127h-26.554l8.047 8.032c0.429 0.444 0.429 1.159 0 1.587-0.444 0.444-1.143 0.444-1.587 0l-9.952-9.952c-0.429-0.429-0.429-1.143 0-1.571l9.952-9.952z" fill="#1E201D"/>
+            </svg>
+          Atgal</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section inner text-content">
+    <div class="container">
+      <div class="inner-hero">
+        <div class="inner-hero__wrap">
+          <div class="inner-hero__bg" style="background-image: url(./assets/images/AnimalShelter.jpg)"></div>
+          <h2  class="inner-hero__title">Naujienos pavadinimas</h2>
+        </div>
+      </div>
+      <div class="text-content-cont">
+        <p><strong>Tiesioginė parama į banko saskaitą. Rekvizitai:</strong></p>
+        <p>VšĮ „Lesė“. Įmonės kodas 300709225</p>
+        <p>IBAN:&nbsp;<strong>LT64 7044 0600 0597 6183</strong>&nbsp;Vilniaus skyrius</p>
+        <p>IBAN:&nbsp;<strong>LT65 7044 0600 0618 6558</strong>&nbsp;Kauno skyriaus sąsk.</p>
+        <p>IBAN:&nbsp;<strong>LT92 7044 0600 0781 2083</strong>&nbsp;Laukinė Lesė</p>
+        <p>SEB bankas, SWIFT kodas CBVILT2X</p>
+        <p><strong>Periodiniai mokėjimai</strong></p>
+        <p>Periodinis mokėjimo pavedimas – nurodymas bankui automatiškai pervesti lėšas kliento nurodytu periodiškumu ir sąlygomis. Tai patogi priemonė klientams, kurie nuolatos turi tam tikru metu sumokėti iš anksto žinomą pinigų sumą ir nori palengvinti kasdieninį finansų tvarkymą.</p>
+        <p>Kaip vykdomi periodiniai mokėjimai?</p>
+        <ul>
+          <li>Kiekvienam mokėjimui sudaroma atskira sutartis, o kiekviena sutartis susiejama su vienu periodiniu mokėjimu, atliekamu jūsų nustatytomis sąlygomis;</li>
+          <li>Bankas jūsų nurodytu periodiškumu automatiškai perves reikiamą sumą iš jūsų banko sąskaitos sutartyje numatytomis sąlygomis. Pasirašius sutartį banke, periodiniai mokėjimai gali būti pradėti jau tą pačią dieną;</li>
+          <li>Mokėjimai atliekami sutarčių sudarymo eilės tvarka, pradedant nuo anksčiausiai sudarytos sutarties;</li>
+          <li>Atėjus mokėjimo dienai, bankas patikrina jūsų banko sąskaitą. Jei lėšų pakanka, pavedimas bus atliktas. Jei mokėjimo dieną sąskaitoje nepakanka lėšų, bankas nuolat tikrins jūsų banko sąskaitą ir, atsiradus reikiamai sumai, atliks mokėjimą;</li>
+          <li>Periodiškai galima atlikti vietinius ir tarptautinius mokėjimus;</li>
+          <li>Prireikus, visuomet galima pakeisti sutartyje nurodytas periodinio mokėjimo sąlygas arba nutraukti mokėjimus.</li>
+        </ul>
+        <p>Mokėjimų periodiškumas gali būti:</p>
+        <ul>
+          <li>nurodant metų mėnesius (kas mėnesį, kas antrą mėnesį, kas ketvirtį, nurodant tam tikrus mėnesius);</li>
+          <li>nurodant mėnesio dieną (kas dieną, nurodant tam tikras mėnesio dienas);</li>
+          <li>nurodant mėnesio savaites (kas savaitę, kiekvieną savaitės dieną, nurodant tam tikras savaitės dienas).</li>
+        </ul>
+        <p><strong>Parama per PayPal sistemą</strong></p>
+        <p>Mus paremti taip pat galite per „PayPal“ mokėjimo sistemą, kuri leidžia kreditinės kortelės tarptautinius pavedimus atlikti nemokamai. „Lesei“, kaip labdaringai organizacijai, taikomi sumažinti pavedimų administravimo mokesčiai (~3%).</p>
+        <p><a href="http://google.lt/">Aukoti per PayPal</a></p>
+      </div>
+    </div>
+  </section>
+  <section class="share">
+
+  </section>
+</main>
+<?php include './partials/footer.php'; ?>
+<?php include './partials/foot.php'; ?>
