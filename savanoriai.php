21da41bc9e80c1598caf77603c99ffe39a64b265
jjakucionis <jjakucionis@Justass-iMac.lan>
2019-10-14 21:34:01 +0300

    Volunteer layout done

---
+++ savanoriai.php
@@ -0,0 +1,60 @@
+<?php include './partials/head.php'; ?>
+<?php include './partials/header.php'; ?>
+<main>
+  <section class="section volunteer volunteer__inner">
+    <div class="section-decoration">
+      <div class="section-decoration__line"></div>
+      <svg class="section-decoration__svg" width="551.06px" height="551.06px" enable-background="new 0 0 551.062 551.062" version="1.1" viewBox="0 0 551.062 551.062" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+        <path d="m465.19 453.46c14.749 67.688-58.752 82.375-91.127 73.562s-98.41-10.281-98.41-10.281-66.218 1.469-98.593 10.281c-32.375 8.874-105.94-5.875-91.249-73.562s79.438-64.75 97.186-156c17.687-91.249 92.718-85.374 92.718-85.374s74.847-5.875 92.535 85.374c17.625 91.249 82.252 88.311 96.94 156zm-121.6-247.31c39.841 11.505 83.844-19.951 98.349-70.258 14.504-50.245-5.998-100.31-45.839-111.81-39.842-11.506-83.844 19.951-98.349 70.258-14.504 50.245 5.998 100.31 45.839 111.81zm165.12-18.298c-38.372-15.668-85.496 10.894-105.26 59.363-19.768 48.471-4.712 100.43 33.66 116.04 38.372 15.606 85.496-10.894 105.26-59.364s4.713-100.37-33.66-116.03zm-301.29 18.298c39.841-11.506 60.343-61.567 45.839-111.81s-58.568-81.702-98.349-70.196c-39.78 11.505-60.343 61.566-45.839 111.81 14.505 50.245 58.508 81.701 98.349 70.196zm-93.453 157.1c38.373-15.667 53.428-67.626 33.66-116.04s-66.892-75.031-105.26-59.363c-38.372 15.667-53.427 67.626-33.659 116.03 19.767 48.47 66.891 75.031 105.26 59.364z"/>
+      </svg>
+      <div class="section-decoration__line"></div>
+    </div>
+    <h2 class="section-title animal-title">Mūsų šaunieji savanoriai</h2>
+    <div class="container">
+      <div class="volunteer-list">
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Tadas</span><br>Savanoris</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+        <div class="volunteer-block">
+          <div class="volunteer-block__image" style="background-image: url(./assets/images/foto.jpg)"></div>
+          <p class="volunteer-block__position"><span class="volunteer-block__position--bolded">Monika</span><br>Savanorė</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php include './partials/footer.php'; ?>
+<?php include './partials/foot.php'; ?>
